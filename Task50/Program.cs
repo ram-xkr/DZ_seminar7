8d3269aae6edae1634e9919a44a21a47a22218ac
ram-xkr <ram-xkr@mail.ru>
2022-10-14 05:14:22 +0300

    Task 50

---
+++ Task50/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции
+// элемента в двумерном массиве, и возвращает значение этого элемента
+// или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+using static System.Console;
+Clear();
+Write("Введите размер матрицы через пробел: ");
+int[] parameters = GetArrayFromString(ReadLine()!);
+int[,] matrix = GetMatrixArray(parameters[0], parameters[1]);
+PrintMatrix(matrix);
+Write("Введите позицию элемента: ");
+int num = int.Parse(ReadLine()!);
+CheckNum(matrix, num);
+
+
+
+int[] GetArrayFromString(string parameters)
+{
+    string[] parames = parameters.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] parameterNum = new int[parames.Length];
+    for (int i = 0; i < parameterNum.Length; i++)
+    {
+        parameterNum[i] = int.Parse(parames[i]);
+    }
+    return parameterNum;
+}
+
+
+int[,] GetMatrixArray(int rows, int columns)
+{
+    int[,] resultMatrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            resultMatrix[i, j] = rnd.Next(0, 100);
+        }
+    }
+    return resultMatrix;
+}
+
+void PrintMatrix(int[,] inMatrix)
+{
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            Write($"{inMatrix[i, j]}\t");
+        }
+        WriteLine();
+    }
+}
+
+void CheckNum(int[,] inMatrix, int number)
+{
+    for (int i = 0; i < inMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < inMatrix.GetLength(1); j++)
+        {
+            if (i == number / 10 && j == number % 10)
+            {
+            Write($"В указанной позиции есть число {inMatrix[i, j]}.");
+            return;
+            }
+        }
+    }
+    Write($"Элемента с таким индексом не существует.");
+}
